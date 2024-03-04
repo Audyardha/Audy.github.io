6c4d327781ce25c03e3357b4ff96a2b38762a450
Audyardha Nasywa Andini <114630414+Audyardha@users.noreply.github.com>
2024-03-04 15:57:52 +0700

    Add files via upload

---
+++ tes.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Penjumlahan
+    var a = 10
+    var b = 5
+    var sum = a + b
+    fmt.Println("Hasil penjumlahan:", sum)
+
+    // Pengurangan
+    var c = 20
+    var d = 8
+    var diff = c - d
+    fmt.Println("Hasil pengurangan:", diff)
+
+	//Perkalian
+	var e = 12
+	var f = 5
+	var product = e * f
+	fmt.Println("Hasil perkalian:", product)
+
+	//Pembagian
+	var g = 50
+	var h = 10
+	var quotient = g / h
+	fmt.Println("Hasil pembagian:", quotient)
+}
+
+
